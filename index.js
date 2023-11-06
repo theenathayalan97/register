7d8a1057d5a253e40a4f997998d26fdf17624d8a
theenathayalan97 <118962794+theenathayalan97@users.noreply.github.com>
2023-11-06 17:42:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 8000 ;
+const port = process.env.PORT || 80 ;
 const mongoose = require("mongoose");
 // const bcrypt = require('bcrypt');
 // const jwt=require('jsonwebtoken');
